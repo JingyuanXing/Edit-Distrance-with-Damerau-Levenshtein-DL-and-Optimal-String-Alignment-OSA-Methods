582ba46cd7717a02c37396bcf4c7473fb2c089b2
JingyuanXing <lucyxing0217@gmail.com>
2019-09-28 18:43:28 -0400

    first commit

---
+++ editdistance.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+class EditDistance():
+	
+	def __init__(self):
+		"""
+		Do not change this
+		"""
+	
+	def calculateLevenshteinDistance(self, str1, str2):
+		"""
+		TODO:
+			take two strings and calculate their Levenshtein Distance for task 1 
+			return an integer which is the distance
+		"""
+		raise NotImplementedError
+
+		
+	def calculateOSADistance(self, str1, str2):
+		"""
+		TODO:
+			take two strings and calculate their OSA Distance for task 2 
+			return an integer which is the distance
+		"""
+		raise NotImplementedError
+
+		
+	def calculateDLDistance(self, str1, str2):
+		"""
+		TODO:
+			take two strings and calculate their DL Distance for task 3 
+			return an integer which is the distance
+		"""
+		raise NotImplementedError
+
---
+++ task4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def task4(dictionary, raw):
+	"""
+	TODO:
+		implement your optimized edit distance function for task 4 here
+		dictionary : path of dictionary.txt file 
+		raw: path of raw.txt file
+		return : a list of min_distance of each word in the raw.txt 
+		compared with words in the dictonary 
+	example return result : [0,1,0,2]
+	"""
+	raise NotImplementedError
+	
