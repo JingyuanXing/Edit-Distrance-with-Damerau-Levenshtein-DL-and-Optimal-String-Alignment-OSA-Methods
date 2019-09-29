6a9c795ed66f6e9e409cdfa6d862030359964f6d
JingyuanXing <lucyxing0217@gmail.com>
2019-09-28 22:10:12 -0400

    Edit distance matrix ready

--- editdistance.py
+++ editdistance.py
@@ -13,7 +13,23 @@ class EditDistance():
 			take two strings and calculate their Levenshtein Distance for task 1 
 			return an integer which is the distance
 		"""
-		raise NotImplementedError
+		editM = [[0 for i in range(len(str1)+1)] for j in range(len(str2)+1)]
+		for i in range(len(str1)+1):
+			for j in range(len(str2)+1):
+				if i == 0 and j == 0:
+					editM[j][i] = 0
+				elif i == 0 and j > 0:
+					editM[j][i] = j
+				elif i > 0 and j == 0:
+					editM[j][i] = i
+				else:
+					if str1[i-1] == str2[j-1]:
+						mismatch = 0
+					else:
+						mismatch = 1
+					editM[j][i] = min(editM[j][i-1]+1, editM[j-1][i]+1, editM[j-1][i-1]+mismatch)
+
+		return editM[len(str2)][len(str1)]
 
 		
 	def calculateOSADistance(self, str1, str2):
@@ -33,3 +49,7 @@ class EditDistance():
 		"""
 		raise NotImplementedError
 
+
+
+ed = EditDistance()
+print(ed.calculateLevenshteinDistance("colleg", "college"))
